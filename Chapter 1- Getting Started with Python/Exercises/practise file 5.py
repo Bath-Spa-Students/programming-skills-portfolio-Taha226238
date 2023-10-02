10aa0bd3bf18062eb27ce07b649e17980483beb7
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-02 11:49:06 +0400

    practice file 5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practise file 5.py
@@ -0,0 +1,11 @@
+# Define four strings in different variables
+str1 = "I,"
+str2 = " Love "
+str3 = " Bath Spa"
+str4 = " University."
+
+# Concatenate the strings using the + operator
+result = str1 + str2 + str3 + str4
+
+# Print the concatenated string
+print(result)
