0a6516b4c09ae26e8f400414db30ac1ef75934cf
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-20 11:40:50 +0400

    exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/excerise 1.py
@@ -0,0 +1,11 @@
+# Assign a message to a variable
+my_message = "Hello, friends!"
+
+# Print the initial message
+print(my_message)
+
+# Change the value of the variable to a new message
+my_message = "This is the new message."
+
+# Print the new message
+print(my_message)
