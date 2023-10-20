c9f7bfb5766dca7d6eb3eda2a642574e40312e86
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-20 12:45:45 +0400

    exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 7.py
@@ -0,0 +1,33 @@
+# Create a list of places to visit
+places_to_visit = ["Paris", "Tokyo", "New York", "Venice", "Sydney"]
+
+# Print the original list
+print("Original List:", places_to_visit)
+
+# Print the sorted list in alphabetical order without modifying the original list
+print("Sorted List (Alphabetical):", sorted(places_to_visit))
+
+# Verify that the original list is still in its original order
+print("Original List:", places_to_visit)
+
+# Print the sorted list in reverse alphabetical order without modifying the original list
+print("Sorted List (Reverse Alphabetical):", sorted(places_to_visit, reverse=True))
+
+# Verify that the original list is still in its original order
+print("Original List:", places_to_visit)
+
+# Use reverse() to change the order of the original list
+places_to_visit.reverse()
+print("Reversed List:", places_to_visit)
+
+# Use reverse() again to change the order back to the original order
+places_to_visit.reverse()
+print("Reversed Back to Original Order:", places_to_visit)
+
+# Use sort() to change the order of the list to alphabetical order
+places_to_visit.sort()
+print("Sorted Using sort() (Alphabetical):", places_to_visit)
+
+# Use sort() to change the order of the list to reverse alphabetical order
+places_to_visit.sort(reverse=True)
+print("Sorted Using sort(reverse=True) (Reverse Alphabetical):", places_to_visit)
