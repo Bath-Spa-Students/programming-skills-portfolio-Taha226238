3ba8c9c0de8aa8074aaad199484fe9623d034353
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-20 11:51:25 +0400

    exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -0,0 +1,6 @@
+# Quote from Albert Einstein
+einstein_quote = 'A person who never made a mistake never tried anything new.'
+author = 'Albert Einstein'
+
+# Print the quote and its author
+print(f'{author} once said, "{einstein_quote}"')
