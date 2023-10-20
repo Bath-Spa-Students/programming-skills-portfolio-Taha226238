85129e1251d015d0e03afc5cdee8388c18deab00
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-20 12:22:29 +0400

    exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 3.py
@@ -0,0 +1,7 @@
+# Create a list of motorcycle brands
+motorcycle_brands = ["Honda", "Yamaha", "Kawasaki", "Ducati", "Harley-Davidson"]
+
+# Print statements about these motorcycles
+for brand in motorcycle_brands:
+    statement = f"I would like to own a {brand} motorcycle."
+    print(statement)
