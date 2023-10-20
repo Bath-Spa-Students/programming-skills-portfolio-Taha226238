c4d7f39b5b5f3b7e6b97c1b1daae52d32fc88f54
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-20 12:03:38 +0400

    exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 5.py
@@ -0,0 +1,13 @@
+# Define the cost of one USB stick and the girl's budget
+usb_stick_cost = 6  # cost of one USB stick in pounds
+budget = 50  # girl's budget in pounds
+
+# Calculate the maximum number of USB sticks she can buy
+num_usb_sticks = budget // usb_stick_cost
+
+# Calculate the remaining pounds after the purchase
+remaining_pounds = budget % usb_stick_cost
+
+# Display the results
+print("The girl can buy", num_usb_sticks, "USB sticks.")
+print("She will have", remaining_pounds, "pounds left.")
