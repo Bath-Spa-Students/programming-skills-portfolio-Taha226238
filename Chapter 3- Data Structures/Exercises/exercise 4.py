9266ec8990bff69dfd6c723d7d4b2d645130d8ea
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-20 12:28:19 +0400

    exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 4.py
@@ -0,0 +1,7 @@
+# Create a list of people to invite to dinner
+guests = ["mary", "rafia", "iftikar"]
+
+# Print invitations to each person
+for guest in guests:
+    invitation = f"Dear {guest}, I would like to invite you to dinner. Please join me for a wonderful evening."
+    print(invitation)
