b09cec9c12dba120d9179127e3fd0689ef7a03bc
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-27 14:38:24 +0400

    exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 1.py
@@ -0,0 +1,9 @@
+alien_color = 'green'  # You can change this value to 'yellow' or 'red' to test different versions
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
+
+alien_color = 'red'  # This version has 'red', which fails the if test
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
