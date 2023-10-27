2a246aa30e3f88aa849c0eca60b8da14e4b6eb3e
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-27 14:49:47 +0400

    exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 2.py
@@ -0,0 +1,8 @@
+alien_color = 'green'  # You can change the color to 'yellow', 'red', or any other color
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+else:
+    print("Congratulations! You just earned 10 points for shooting the non-green alien.")
+
+
