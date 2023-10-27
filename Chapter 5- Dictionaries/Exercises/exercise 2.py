218c4c234bcd4fe9f8a976ce3358e33909002dcc
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-10-27 15:05:36 +0400

    exercise 2.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise 2.py
@@ -0,0 +1,11 @@
+programming_glossary = {
+    'variable': 'A named storage location in a program that can hold data or values.',
+    'function': 'A reusable block of code that performs a specific task.',
+    'loop': 'A control structure that allows a set of instructions to be executed repeatedly.',
+    'list': 'An ordered collection of items, which can be of different types.',
+    'conditional statement': 'A statement that performs different actions depending on whether a condition is True or False.'
+}
+
+# Print each word and its meaning with a newline between each pair
+for word, meaning in programming_glossary.items():
+    print(f"{word}:\n{meaning}\n")
