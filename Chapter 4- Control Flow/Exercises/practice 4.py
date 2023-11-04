13dd38987bcdb18c290d9c660ca58dbdf87eb6e8
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-04 18:59:15 +0400

    practice 4.py

---
+++ Chapter 4- Control Flow/Exercises/practice 4.py
@@ -0,0 +1,7 @@
+# Assuming you have a variable named "speed" with a value
+speed = 40  # Replace with the actual value of "speed"
+
+if 24 <= speed <= 56:
+    print("Speed is normal")
+else:
+    print("Speed is abnormal")
