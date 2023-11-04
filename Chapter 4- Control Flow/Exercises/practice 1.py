0c9b001f0f494ff376e6aa89c54ec85f852c1374
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-04 18:45:46 +0400

    practice 1.py

---
+++ Chapter 4- Control Flow/Exercises/practice 1.py
@@ -0,0 +1,14 @@
+# Assuming you have a variable named "x" with a value
+
+x = 120  # You can replace this with your actual value for "x"
+
+if x > 100:
+    y = 20
+    z = 40
+else:
+    # Assign some other values to y and z if x is not greater than 100
+    y = 10  # Example default value for y
+    z = 30  # Example default value for z
+
+print("y =", y)
+print("z =", z)
