db63865b08039bb7f98827e2e53651bbbdeda667
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-04 17:19:52 +0400

    practice file4.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice 4.py
@@ -0,0 +1,6 @@
+x = 42  # An integer
+y = "Hello, world!"  # A string
+
+# Print the types of variables x and y
+print(f"Variable x is of type: {type(x)}")
+print(f"Variable y is of type: {type(y)}")
