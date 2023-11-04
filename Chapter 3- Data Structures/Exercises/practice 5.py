5ab4d76cb191978afe62975c97cdd85500a1a60c
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-04 18:30:21 +0400

    practice 3.py

---
+++ Chapter 3- Data Structures/Exercises/practice 5.py
@@ -0,0 +1,17 @@
+list1 = [5, 10, 15, 20, 25, 50, 20]
+value_to_find = 20
+replacement_value = 200
+found = False
+
+for i in range(len(list1)):
+    if list1[i] == value_to_find:
+        list1[i] = replacement_value
+        found = True
+        break
+
+if found:
+    print("Value 20 has been replaced with 200:")
+else:
+    print("Value 20 not found in the list.")
+
+print(list1)
