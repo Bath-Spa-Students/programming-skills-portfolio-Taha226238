aa24f61bbd25475bea6a2e9a017e6d722cd31839
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-04 17:22:34 +0400

    practice file 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice file 5.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+
+# Unpack the list into variables x, y, and z
+x, y, z = fruits
+
+# Print the values of x, y, and z
+print("Value of x:", x)
+print("Value of y:", y)
+print("Value of z:", z)
