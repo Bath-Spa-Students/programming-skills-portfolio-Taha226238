1e0eb179548a2943a266da65a337a1f8c128435a
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-04 18:28:34 +0400

    practice 4.py

---
+++ Chapter 3- Data Structures/Exercises/practice 4.py
@@ -0,0 +1,7 @@
+numbers1 = [1, 2, 3, ...]  # Assuming numbers1 has 100 elements
+numbers2 = []  # Initialize an empty list
+
+for number in numbers1:
+    numbers2.append(number)
+
+# Now, numbers2 contains the same values as numbers1
