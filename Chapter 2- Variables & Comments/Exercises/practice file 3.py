7e3ddb801f08dbc4f694d0d15a53d27758e353be
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-04 17:15:23 +0400

    practice file 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice file 3.py
@@ -0,0 +1,12 @@
+# Input from the user
+user_input = input("Enter a value: ")
+
+# Typecast the input to string, int, and float
+as_string = str(user_input)
+as_integer = int(user_input)
+as_float = float(user_input)
+
+# Print the variables
+print(f"Input as string: {as_string}")
+print(f"Input as integer: {as_integer}")
+print(f"Input as float: {as_float}")
