70f3d303bfddcb09ccb3ab38da4e876836887e10
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 11:09:45 +0400

    practice 4.py

---
+++ Chapter 5- Dictionaries/Exercises/practice 4.py
@@ -0,0 +1,11 @@
+# Define a sample dictionary
+my_dictionary = {
+    "name": "Taha khan",
+    "age": 18 ,
+    "occupation": "Creative Computing",
+    "city": "Ajman"
+}
+
+# Iterate through both keys and values and print them
+for key, value in my_dictionary.items():
+    print(f"Key: {key}, Value: {value}")
