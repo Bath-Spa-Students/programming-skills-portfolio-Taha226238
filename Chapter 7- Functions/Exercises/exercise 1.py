9a5d3cea902970775a6b205f0615e9faed25baeb
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 12:19:03 +0400

    exercise 1.py

--- Chapter 7- Functions/Exercises/exercise 1.py
+++ Chapter 7- Functions/Exercises/exercise 1.py
@@ -2,4 +2,4 @@ def display_message(chapter_title, message):
     print(f"In the chapter '{chapter_title}', we are learning about {message}.")
 
 # Call the function with the chapter title and the message
-display_message("Chapter 3", "Python functions and how to use them.")
+display_message("Chapter 7", "Python functions and how to use them.")
