75243e5517022d6b150a24cc9231e036fcecbe64
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 09:01:07 +0400

    exercise 4.py

--- Chapter 1- Getting Started with Python/Exercises/excerise 4.py
+++ Chapter 1- Getting Started with Python/Exercises/excerise 4.py
@@ -1,7 +1,7 @@
 # Create three strings in different variables
 string1 = "Hello, "
-string2 = "world! "
-string3 = "How are you?"
+string2 = "i am taha  "
+string3 = "i love bathspa"
 
 # Concatenate the strings into one string
 result_string = string1 + string2 + string3
