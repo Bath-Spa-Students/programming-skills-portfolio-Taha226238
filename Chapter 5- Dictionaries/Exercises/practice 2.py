47495fcf9c73d59fcb56a0b80dc41f96315025f1
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 11:02:00 +0400

    practice 2.py

---
+++ Chapter 5- Dictionaries/Exercises/practice 2.py
@@ -0,0 +1,11 @@
+# Define a sample dictionary
+my_dictionary = {
+    "name": "taha",
+    "age": 18,
+    "occupation": "creative computing",
+    "city": "Ajman"
+}
+
+# Iterate through the keys and print them
+for key in my_dictionary:
+    print(key)
