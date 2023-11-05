87108ecf7b0e37b199933de0e770110e118723b7
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 11:06:46 +0400

    practice 3.py

---
+++ Chapter 5- Dictionaries/Exercises/practice 3.py
@@ -0,0 +1,11 @@
+# Define a sample dictionary
+my_dictionary = {
+    "name": "Taha khan ",
+    "age": 18,
+    "occupation": "creative computing",
+    "city": "Ajman"
+}
+
+# Iterate through the values and print them
+for value in my_dictionary.values():
+    print(value)
