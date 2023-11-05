1c4cb4ca91ee463356c2c65acf5f4d0f3eeaf244
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 12:22:39 +0400

    exercise 2.py

--- Chapter 7- Functions/Exercises/exercise 2.py
+++ Chapter 7- Functions/Exercises/exercise 2.py
@@ -2,4 +2,4 @@ def favorite_book(title):
     print(f"One of my favorite books is {title}.")
 
 # Call the function with a book title as an argument
-favorite_book("Quran")
+favorite_book("tHEN She Was Gone")
