f0976e27be231e48310b2332b58775285a86692e
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 11:14:49 +0400

    practice 5.py

---
+++ Chapter 5- Dictionaries/Exercises/practice 5.py
@@ -0,0 +1,8 @@
+# Define two dictionaries
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+
+# Merge the dictionaries
+merged_dict = {**dict1, **dict2}
+
+print("Merged Dictionary:", merged_dict)
