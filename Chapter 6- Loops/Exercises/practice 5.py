785551aad37cf948b511f83224c16425ff690696
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 12:10:34 +0400

    exercise 5.py

---
+++ Chapter 6- Loops/Exercises/practice 5.py
@@ -0,0 +1,19 @@
+largest = None  # Initialize the variable to store the largest number
+
+while True:
+    user_input = input("Enter a number (or 0 to exit): ")
+
+    if user_input == '0':
+        break  # Exit the loop if the user enters '0'
+
+    try:
+        number = int(user_input)  # Convert the user input to an integer
+        if largest is None or number > largest:
+            largest = number  # Update the largest number if needed
+    except ValueError:
+        print("Invalid input. Please enter a number or '0' to exit.")
+
+if largest is not None:
+    print(f"The largest number entered is: {largest}")
+else:
+    print("No valid numbers were entered.")
