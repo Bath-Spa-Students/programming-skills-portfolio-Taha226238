6d36ee795e033e00d130142b6ca1031bb9fac63b
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 12:34:42 +0400

    exercise 5.py

--- Chapter 7- Functions/Exercises/exercise 5.py
+++ Chapter 7- Functions/Exercises/exercise 5.py
@@ -1,7 +1,7 @@
-def describe_city(city, country="USA"):
+def describe_city(city, country="UAE"):
     print(f"{city} is in {country}.")
 
 # Call the function for three different cities, including one not in the default country
-describe_city("New York")
-describe_city("Reykjavik", "Iceland")
-describe_city("Paris", "France")
+describe_city("Ajman")
+describe_city("Dubai", "UAE")
+describe_city("AL AIN", "UAE")
