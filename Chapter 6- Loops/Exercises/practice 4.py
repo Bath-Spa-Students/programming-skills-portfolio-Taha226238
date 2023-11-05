86bdbe0549e4b97ebd70030e7c3b64aecaf7fc4f
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 12:09:11 +0400

    practice 4.py

---
+++ Chapter 6- Loops/Exercises/practice 4.py
@@ -0,0 +1,8 @@
+target_value = 10
+
+for number in range(1, 11):
+    if number == target_value:
+        print(f"Found the target value: {target_value}")
+        break  # Exit the loop when the condition is met
+    else:
+        print(f"Current number: {number}")
