03dfc268ef6f34111c94a2dd4d12a772738ab555
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 12:30:01 +0400

    exercise 3.py

--- Chapter 7- Functions/Exercises/exercise 3.py
+++ Chapter 7- Functions/Exercises/exercise 3.py
@@ -2,7 +2,7 @@ def make_shirt(size, message):
     print(f"Creating a shirt of size {size} with the message: '{message}'")
 
 # Call the function using positional arguments
-make_shirt("Medium", "I love Python")
+make_shirt("Medium", "I love my class")
 
 # Call the function using keyword arguments
-make_shirt(size="Large", message="Python is awesome!")
+make_shirt(size="Large", message="bath spa is awesome!")
