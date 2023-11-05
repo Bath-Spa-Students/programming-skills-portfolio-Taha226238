756058b9e2fb8a8ab13f42b975a660122bf6e436
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 11:31:45 +0400

    exercise 1.py

--- Chapter 6- Loops/Exercises/exercise 1.py
+++ Chapter 6- Loops/Exercises/exercise 1.py
@@ -1,21 +1,14 @@
+# Initialize an empty list to store pizza toppings
+pizza_toppings = []
+
 while True:
     topping = input("Enter a pizza topping (or 'quit' to finish): ")
     
     if topping.lower() == 'quit':
-<<<<<<< HEAD
-<<<<<<< HEAD
-        print("Quitting the pizza topping selection process.")
-        break
+        break  # Exit the loop if 'quit' is entered
     else:
-        print(f"I'll add {topping} to your pizza.")
-=======
-=======
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
-        print("Thank you for your order. Your pizza is ready!")
-        break
-    else:
-        print(f"You'll add {topping} to your pizza.")
-<<<<<<< HEAD
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
-=======
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
+        pizza_toppings.append(topping)
+
+# Print the list of pizza toppings
+print("Your pizza toppings are:", ", ".join(pizza_toppings))
+
