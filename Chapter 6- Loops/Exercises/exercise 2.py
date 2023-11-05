27b610ff91bb13bda9fd5ba26e193bff36bef2a4
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 11:32:23 +0400

    exercise 2.py

--- Chapter 6- Loops/Exercises/exercise 2.py
+++ Chapter 6- Loops/Exercises/exercise 2.py
@@ -1,40 +1,18 @@
 while True:
-<<<<<<< HEAD
-<<<<<<< HEAD
+    age_input = input("Please enter your age (or 'quit' to exit): ")
+    
+    if age_input.lower() == 'quit':
+        break  # Exit the loop if the user enters 'quit'
+    
     try:
-        age = int(input("Please enter your age (or enter '0' to quit): "))
-        
-        if age == 0:
-            print("Exiting the ticket pricing system.")
-            break
-        elif age < 3:
+        age = int(age_input)
+        if age < 3:
             ticket_price = 0
-        elif age >= 3 and age <= 12:
+        elif 3 <= age <= 12:
             ticket_price = 10
         else:
             ticket_price = 15
         
-        print(f"The cost of your movie ticket is ${ticket_price}")
-=======
-=======
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
-    age_str = input("Please enter your age (or 'quit' to exit): ")
-
-    if age_str.lower() == 'quit':
-        break
-
-    try:
-        age = int(age_str)
-
-        if age < 3:
-            print("Your ticket is free.")
-        elif 3 <= age <= 12:
-            print("Your ticket costs $10.")
-        else:
-            print("Your ticket costs $15.")
-<<<<<<< HEAD
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
-=======
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
+        print(f"Your ticket price is ${ticket_price}")
     except ValueError:
         print("Please enter a valid age as a number.")
