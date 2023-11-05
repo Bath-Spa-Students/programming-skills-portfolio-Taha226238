b7496d43b292af3851421bca88f5a75d63997cb3
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-05 11:40:47 +0400

    exercise 3.py

--- Chapter 6- Loops/Exercises/exercise 3.py
+++ Chapter 6- Loops/Exercises/exercise 3.py
@@ -1,10 +1,4 @@
 while True:
-<<<<<<< HEAD
-<<<<<<< HEAD
-    pass  # This is an empty loop that will run indefinitely
-=======
-=======
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
     age = input("How old are you?" )
     
     age = int(age)
@@ -13,9 +7,4 @@ while True:
     elif age < 12:
         print("your ticket is $10") 
     else:
-<<<<<<< HEAD
         print("your ticket is $15")
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
-=======
-        print("your ticket is $15")
->>>>>>> a647256d6b4e38deb229fb3ad9b91c786b2b6d31
