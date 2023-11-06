46f49a84c5fd84a9ee81abbd997034e754116d0b
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-06 10:44:33 +0400

    Update practice 4.py

--- Chapter 3- Data Structures/Exercises/practice 4.py
+++ Chapter 3- Data Structures/Exercises/practice 4.py
@@ -1,5 +1,5 @@
 numbers1 = [1, 2, 3, ...]  # Assuming numbers1 has 100 elements
-numbers2 = []  # Initialize an empty list
+numbers2 = [2]  # Initialize an empty list
 
 for number in numbers1:
     numbers2.append(number)
