f32fd328a394d22778238870f65e93f9f1c55706
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-12 22:18:18 +0400

    exercise 4.py

--- Chapter 4- Control Flow/Exercises/exercise4.py
+++ Chapter 4- Control Flow/Exercises/exercise4.py
@@ -1,14 +1,14 @@
 age = 25  # You can change the age to test different stages of life
 
 if age < 2:
-    print("The person is a baby.")
+    print("The person is baby.")
 elif age < 4:
-    print("The person is a toddler.")
+    print("The person is toddler.")
 elif age < 13:
-    print("The person is a kid.")
+    print("The person is  kid.")
 elif age < 20:
-    print("The person is a teenager.")
+    print("The person is  teenager.")
 elif age < 65:
-    print("The person is an adult.")
+    print("The person is  adult.")
 else:
-    print("The person is an elder.")
+    print("The person is elder.")
