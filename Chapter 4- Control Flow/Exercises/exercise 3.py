6f8ddf99b9342abba912defe656a4fa9f7e0caa5
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-12 22:15:44 +0400

    exercise 3.py

--- Chapter 4- Control Flow/Exercises/exercise 3.py
+++ Chapter 4- Control Flow/Exercises/exercise 3.py
@@ -1,26 +1,26 @@
 alien_color = 'green'
 
 if alien_color == 'green':
-    print("Congratulations! You just earned 5 points for shooting the green alien.")
+    print("Congratulations! You have just earned 5 points for shooting the green alien.")
 elif alien_color == 'yellow':
-    print("Congratulations! You just earned 10 points for shooting the yellow alien.")
+    print("Congratulations! You have just earned 10 points for shooting the yellow alien.")
 else:
-    print("Congratulations! You just earned 15 points for shooting the red alien.")
+    print("Congratulations! You have just earned 15 points for shooting the red alien.")
 
 alien_color = 'yellow'
 
 if alien_color == 'green':
-    print("Congratulations! You just earned 5 points for shooting the green alien.")
+    print("Congratulations! You have just earned 5 points for shooting the green alien.")
 elif alien_color == 'yellow':
-    print("Congratulations! You just earned 10 points for shooting the yellow alien.")
+    print("Congratulations! You have just earned 10 points for shooting the yellow alien.")
 else:
-    print("Congratulations! You just earned 15 points for shooting the red alien.")
+    print("Congratulations! You have just earned 15 points for shooting the red alien.")
 
 alien_color = 'red'
 
 if alien_color == 'green':
-    print("Congratulations! You just earned 5 points for shooting the green alien.")
+    print("Congratulations! You have just earned 5 points for shooting the green alien.")
 elif alien_color == 'yellow':
-    print("Congratulations! You just earned 10 points for shooting the yellow alien.")
+    print("Congratulations! You have just earned 10 points for shooting the yellow alien.")
 else:
-    print("Congratulations! You just earned 15 points for shooting the red alien.")
+    print("Congratulations! You have just earned 15 points for shooting the red alien.")
