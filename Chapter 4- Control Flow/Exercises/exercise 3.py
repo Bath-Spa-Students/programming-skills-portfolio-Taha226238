b38de6cf926a3627a9525b72a9326ceed194c6ce
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-14 19:41:18 +0400

    exercise 3.py

--- Chapter 4- Control Flow/Exercises/exercise 3.py
+++ Chapter 4- Control Flow/Exercises/exercise 3.py
@@ -10,17 +10,17 @@ else:
 alien_color = 'yellow'
 
 if alien_color == 'green':
-    print("Congratulations! You have just earned 5 points for shooting the green alien.")
+    print("Congratulations! You have earned 5 points for shooting the green alien.")
 elif alien_color == 'yellow':
-    print("Congratulations! You have just earned 10 points for shooting the yellow alien.")
+    print("Congratulations! You have earned 10 points for shooting the yellow alien.")
 else:
-    print("Congratulations! You have just earned 15 points for shooting the red alien.")
+    print("Congratulations! You have earned 15 points for shooting the red alien.")
 
 alien_color = 'red'
 
 if alien_color == 'green':
-    print("Congratulations! You have just earned 5 points for shooting the green alien.")
+    print("Congratulations! You have earned 5 points for shooting the green alien.")
 elif alien_color == 'yellow':
-    print("Congratulations! You have just earned 10 points for shooting the yellow alien.")
+    print("Congratulations! You have earned 10 points for shooting the yellow alien.")
 else:
-    print("Congratulations! You have just earned 15 points for shooting the red alien.")
+    print("Congratulations! You have earned 15 points for shooting the red alien.")
