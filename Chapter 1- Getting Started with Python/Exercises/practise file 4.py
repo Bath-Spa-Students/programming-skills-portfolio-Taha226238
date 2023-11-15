b0eaea46ffbed1ed9fe08619cd93528b602706ad
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-15 16:51:20 +0400

    practice 4.py

--- Chapter 1- Getting Started with Python/Exercises/practise file 4.py
+++ Chapter 1- Getting Started with Python/Exercises/practise file 4.py
@@ -1,9 +1,9 @@
 # Input the base and height of the triangle
-base = float(input("Enter the length of the base of the triangle: "))
-height = float(input("Enter the height of the triangle: "))
+base = float(input("Enter the length of a base of the triangle: "))
+height = float(input("Enter the height of a the triangle: "))
 
 # Calculate the area of the triangle
 area = 0.5 * base * height
 
 # Print the result
-print("The area of the triangle is:", area)
+print("The area of triangle is:", area)
