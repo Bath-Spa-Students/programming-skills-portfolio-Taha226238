c0f5364380ae4622868798b9224cd1194a628610
Taha226238 <144995443+Taha226238@users.noreply.github.com>
2023-11-15 17:03:44 +0400

    practice 2.py

--- Chapter 2- Variables & Comments/Exercises/practice file 2.py
+++ Chapter 2- Variables & Comments/Exercises/practice file 2.py
@@ -11,21 +11,21 @@ def calculate_percentage(course_marks, total_marks):
     return percentage
 
 # Input the number of courses and individual course marks
-num_courses = int(input("Enter  number of courses: "))
+num_courses = int(input("Enter the number of courses: "))
 course_marks = []
 
 for i in range(num_courses):
-    mark = float(input(f"Enter the marks of the course {i+1}: "))
+    mark = float(input(f"Enter the marks of  course {i+1}: "))
     course_marks.append(mark)
 
 # You can either set the total marks to 500 or take it as input from the user
 # total_marks = 500  # You can uncomment this line if you want to use a fixed total marks
 
 # Alternatively, you can take total marks from the user as input
-total_marks = float(input("Enter total marks: "))
+total_marks = float(input("Enter the total marks: "))
 
 average = calculate_average(course_marks)
 percentage = calculate_percentage(course_marks, total_marks)
 
-print(f"Average of course marks: {average}")
+print(f"Average of the course marks: {average}")
 print(f"Percentage: {percentage}%")
